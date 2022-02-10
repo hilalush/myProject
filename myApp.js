f6ba6a4ae3884c69c7b3bc6cf11517b76a54e80d
student <student@BA.local>
2022-02-10 11:10:05 +0200

    no chang

---
+++ myApp.js
@@ -0,0 +1,7 @@
+function change(){
+    document.getElementById("1").innerHTML="sucsses"
+    document.getElementById("2").innerHTML+=" hila"
+    document.body.style.backgroundColor="pink"
+    document.body.style.textAlign="center"
+    document.body.style.marginTop="20%"
+}
